6e9472eb95d0d7653d232ec513fc7cff8d0a091e
Paula Cano <paulacanoroman@gmail.com>
2024-07-17 17:43:05 +0200

    feat: delete letter use replaceAll

--- src/components/DeletLetter.tsx
+++ src/components/DeletLetter.tsx
@@ -4,14 +4,20 @@ import { useState } from "react"
 
 const DeletLetter = () => {
 
-    const [userSentence, setUserSentence] = useState<string>()
+    const [userSentence, setUserSentence] = useState<string>('')
+    const [userLetter, setUserLetter] = useState<string>('')
+
+    const handleDelLet = ()=>{
+        const delLet = userSentence.replaceAll(userLetter, '')
+        setUserSentence(delLet)
+    }
+
   return (
     <div>
-
         <input placeholder="Write your sentence" onChange={(e)=>{setUserSentence(e.target.value)}}></input>
-        <input placeholder="Write one letter" type="text" maxLength={1} size={1}></input>
+        <input placeholder="Write one letter" type="text" maxLength={1} size={1} onChange={(e)=>{setUserLetter(e.target.value)}}></input>
+        <button onClick={()=>{handleDelLet()}}>click for delete</button>
         {userSentence}
-
     </div>
   )
 }
