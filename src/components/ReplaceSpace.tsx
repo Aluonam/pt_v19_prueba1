ca959ceaa0960bce9f9a86885a71268530549e91
Paula Cano <paulacanoroman@gmail.com>
2024-07-17 16:56:38 +0200

    feat: replace space

--- src/components/ReplaceSpace.tsx
+++ src/components/ReplaceSpace.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react"
 
 
 const ReplaceSpace = () => {
+
+    const [userSentence, setUserSentence] = useState<string>()
+
+    const handleChangeSentence = ()=>{
+        const newSentece = userSentence?.split(' ').join('_')
+        setUserSentence(newSentece)
+    }
+     
+
   return (
     <>
-    <input placeholder="Write your sentence" onChange={(e)=>{console.log(e.target.value)}}></input>
+    <div>
+        <input placeholder="Write your sentence" onChange={(e)=>{setUserSentence(e.target.value)}}></input>
+        <button onClick={()=>{handleChangeSentence()}}>change space</button>
+        <br></br>
+        {userSentence}
+    </div>
+    
     </>
   )
 }
