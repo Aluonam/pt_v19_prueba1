52b32d611122d0a8855aea5b7c00c5d266c505fd
Paula Cano <paulacanoroman@gmail.com>
2024-07-17 17:04:28 +0200

    feat: state userSentence

--- src/components/DeletLetter.tsx
+++ src/components/DeletLetter.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react"
 
 
 
 const DeletLetter = () => {
+
+    const [userSentence, setUserSentence] = useState<string>()
   return (
     <div>
 
-        <input placeholder="Write your sentence" onChange={(e)=>{e.target.value}}></input>
+        <input placeholder="Write your sentence" onChange={(e)=>{setUserSentence(e.target.value)}}></input>
         <input placeholder="Write one letter" type="text" maxLength={1} size={1}></input>
+        {userSentence}
 
     </div>
   )
